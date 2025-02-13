82c29b4ee60580a86807b2109f0e04823b5fbfa4
jenyakrol <jenyakrol@yandex.ru>
2025-02-13 11:09:36 +0300

    modal 3

--- src/scripts/forms.js
+++ src/scripts/forms.js
@@ -3,12 +3,23 @@ function editFormSubmit(name, description, newName, newDescription) {
   description.textContent = newDescription;
 }
 
-function addNewPlace(newName, newLink, container, cardTemplate, createCard) {
+function addNewPlace(
+  newName,
+  newLink,
+  container,
+  cardTemplate,
+  createCard,
+  deleteCard,
+  likeCard,
+  openImagePopup
+) {
   if (!newName || !newLink) {
     return;
   }
   const item = { name: newName, link: newLink };
-  container.prepend(createCard(item, cardTemplate));
+  container.prepend(
+    createCard(item, cardTemplate, deleteCard, likeCard, openImagePopup)
+  );
 }
 
 export { editFormSubmit, addNewPlace };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -20,14 +20,14 @@ const popups = document.querySelectorAll(".popup");
 const editButton = document.querySelector(".profile__edit-button");
 const editPopup = document.querySelector(".popup_type_edit");
 const editForm = document.forms["edit-profile"];
-const editName = editForm.elements.name;
-const editDescription = editForm.elements.description;
+const inputNameProfile = editForm.elements.name;
+const inputDescriptionProfile = editForm.elements.description;
 
 const addButton = document.querySelector(".profile__add-button");
 const addPopup = document.querySelector(".popup_type_new-card");
 const addForm = document.forms["new-place"];
-const newPlace = addForm.elements["place-name"];
-const newImage = addForm.elements.link;
+const inputNameAddNewCard = addForm.elements["place-name"];
+const inputLinkAddNewCard = addForm.elements.link;
 
 const imagePopup = document.querySelector(".popup_type_image");
 const imageInPopup = imagePopup.querySelector(".popup__image");
@@ -52,8 +52,8 @@ popups.forEach((popup) => {
 });
 
 editButton.addEventListener("click", (evt) => {
-  editName.value = profileName.textContent;
-  editDescription.value = profileDescription.textContent;
+  inputNameProfile.value = profileName.textContent;
+  inputDescriptionProfile.value = profileDescription.textContent;
   openPopup(editPopup);
 });
 editForm.addEventListener("submit", (evt) => {
@@ -61,8 +61,8 @@ editForm.addEventListener("submit", (evt) => {
   editFormSubmit(
     profileName,
     profileDescription,
-    editName.value,
-    editDescription.value
+    inputNameProfile.value,
+    inputDescriptionProfile.value
   );
   closePopup(editPopup);
 });
@@ -73,11 +73,14 @@ addButton.addEventListener("click", (evt) => {
 addForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
   addNewPlace(
-    newPlace.value,
-    newImage.value,
+    inputNameAddNewCard.value,
+    inputLinkAddNewCard.value,
     cardsConatiner,
     cardTemplate,
-    createCard
+    createCard,
+    deleteCard,
+    likeCard,
+    openImagePopup
   );
   addForm.reset();
   closePopup(addPopup);
